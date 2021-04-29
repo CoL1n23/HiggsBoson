d861a03573f823828121a2752fd90e32dbb72e51
Colin Wu <colinwu1418@gmail.com>
2021-04-29 18:04:55 -0400

    finished training

--- main.py
+++ main.py
@@ -53,11 +53,7 @@ def run():
     train_samples = positive_samples[:len(positive_samples)/2] + negative_samples[:len(negative_samples)/2]
     validation_samples = positive_samples[len(positive_samples)/2:] + negative_samples[len(negative_samples)/2:]
 
-    B = 10
-    best_F_1 = 1
-    best_F_2 = 1
-    best_k_1 = 0
-    best_k_2 = 0
+    B = 1
     best_err_1 = 1.5
     best_err_2 = 1.5
 
@@ -65,9 +61,11 @@ def run():
     X_fold2 = X[validation_samples]
 
     # Hyperparameter tuning for F and k
-    all_combinations = np.zeros((10, 10))
-    for F in range(10, 20):
-        for k in range(20, 30):
+    all_combinations = np.zeros((7, 20))
+    for F in range(8, 15):
+        k_list = list(range(10, 30))
+
+        for k in k_list:
             print "Current F and k: " + str(F) + " " + str(k)
 
             # Do PCA on both folds
@@ -80,23 +78,18 @@ def run():
             err1 = bootstrapping.run(B, X_fold1_small, y[train_samples], k)
             if err1 < best_err_1:
                 best_err_1 = err1
-                best_k_1 = k
-                best_F_1 = F
 
             err2 = bootstrapping.run(B, X_fold2_small, y[validation_samples], k)
             if err2 < best_err_2:
                 best_err_2 = err2
-                best_k_2 = k
-                best_F_2 = F
 
-            all_combinations[F - 10][k - 20] = (err1 + err2) / 2
-    print "Final result is : " + str(best_k_1) + " " + str(best_k_2) + " " + str(best_err_1) + " " + str(
-        best_err_2) + " " + str(best_F_1) + " " + str(best_F_2)
+            all_combinations[F - 8][k - 10] = (err1 + err2) / 2
 
     # Retrieve the best F and k as the tuning result
     best_err = np.amin(all_combinations)
-    best_F = np.where(all_combinations == best_err)[0][0] + 10
-    best_k = np.where(all_combinations == best_err)[1][0] + 20
+    best_F = np.where(all_combinations == best_err)[0][0] + 8
+    best_k = np.where(all_combinations == best_err)[1][0] + 10
+    print "Obtained result! best_F=" + str(best_F) + ", best_k=" + str(best_k)
 
     # Compute classification error for the best F and k
     y_pred = np.zeros(len(X), int)
@@ -105,7 +98,7 @@ def run():
     X_fold2_small = pcaproj.run(X_fold2, mu_fold1, Z_fold1)
 
     alg = KNeighborsClassifier(n_neighbors=best_k, algorithm='brute')
-    alg.fit(X_fold1_small, y[train_samples])
+    alg.fit(X_fold1_small, np.ravel(y[train_samples]))
     y_pred[validation_samples] = alg.predict(X_fold2_small)
 
     mu_fold2, Z_fold2 = pcalearn.run(best_F, X_fold2)
@@ -113,14 +106,19 @@ def run():
     X_fold2_small = pcaproj.run(X_fold2, mu_fold2, Z_fold2)
 
     alg = KNeighborsClassifier(n_neighbors=best_k, algorithm='brute')
-    alg.fit(X_fold2_small, y[validation_samples])
+    alg.fit(X_fold2_small, np.ravel(y[validation_samples]))
     y_pred[train_samples] = alg.predict(X_fold1_small)
 
-    err = np.mean(y != y_pred)
+    err = np.mean(y != np.array([y_pred]).T)
 
     print "Classification Error:" + str(err)
-    print len(positive_samples)
-    print len(negative_samples)
+    desired1 = float(len(positive_samples)) / len(X)
+    desired2 = float(len(negative_samples)) / len(X)
+    print "Desired classification error=" + str(min(desired1, desired2))
+    if err < min(desired1, desired2):
+        print "Congrats! The training is successful!"
+    else:
+        print "Sorry, the model needs to be improved..."
 
 
 if __name__ == '__main__':
